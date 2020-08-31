52f5a0d51395aa6823f0c11fce77db42ffd16e03
JJuanC <camiloflstudio@gmail.com>
2020-08-31 14:22:12 -0500

    Updates

---
+++ precache-manifest.f1e1060b8135e1717d2da91e98180de6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a13c5baa2aa04406abe0b9186e9f25b",
+    "url": "/Camilo95VP/Drums_machine/index.html"
+  },
+  {
+    "revision": "243d94b9e927a74356f7",
+    "url": "/Camilo95VP/Drums_machine/static/css/main.d18195c7.chunk.css"
+  },
+  {
+    "revision": "8bf8c8ba1520c53ba6d6",
+    "url": "/Camilo95VP/Drums_machine/static/js/2.58b9601a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Camilo95VP/Drums_machine/static/js/2.58b9601a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "243d94b9e927a74356f7",
+    "url": "/Camilo95VP/Drums_machine/static/js/main.fe643626.chunk.js"
+  },
+  {
+    "revision": "42e16453599d10681425",
+    "url": "/Camilo95VP/Drums_machine/static/js/runtime-main.57a21260.js"
+  },
+  {
+    "revision": "eb31578a128f05df35c1aa2de7e8cc71",
+    "url": "/Camilo95VP/Drums_machine/static/media/thumb-1920-474157.eb31578a.jpg"
+  }
+]);
